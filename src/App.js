7d72b1194af817f9c4f0042b8b0eedf93b714c08
catonlan <catonlan@163.com>
2019-09-10 14:41:27 +0800

    add 首页侧边栏

--- src/App.js
+++ src/App.js
@@ -1,13 +1,64 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Drawer, List, NavBar, Icon, } from 'antd-mobile';
+import './App.less';
+import thumbImg from './assets/image/gray.png';
 
-function App() {
-  return (
-    <div className="App">
+class App extends React.Component {
+  state = {
+    open: true,
+  }
+  onOpenChange = (...args) => {
+    console.log(args);
+    this.setState({open: !this.state.open, });
+  }
 
-    </div>
-  );
+  render() {
+    const menus = [
+      {
+        title: '首页',
+        path: '/',
+        thumb: thumbImg,
+      },
+      {
+        title: '热门',
+        path: '/popular',
+        thumb: thumbImg,
+      },
+      {
+        title: '搜索',
+        path: '/search',
+        thumb: thumbImg,
+      },
+
+    ];
+
+    const sidebar = (<List>
+      {menus.map((menu, index) => {
+        return (<List.Item key={index}
+          thumb={menu.thumb}
+        > {menu.title} </List.Item>);
+      })}
+    </List>);
+
+    return (
+      <div>
+        <NavBar icon={<Icon type="ellipsis" />}
+          onLeftClick={this.onOpenChange}
+        >Battle</NavBar>
+        <Drawer
+          className="my-drawer"
+          contentStyle={{color: '#A6A6A6', textAlign: 'center', paddingTop: 42, }}
+          enableDragHandle
+          onOpenChange={this.onOpenChange}
+          open={this.state.open}
+          sidebar={sidebar}
+          style={{minHeight: document.documentElement.clientHeight, }}
+        >
+          Github battle
+        </Drawer>
+      </div>
+    );
+  }
 }
 
 export default App;
