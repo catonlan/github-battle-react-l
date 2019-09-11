fc6798e7ab615392b4fd0a07facdbf5651ff11fb
catonlan <catonlan@163.com>
2019-09-11 10:52:45 +0800

    fix

---
+++ src/components/Footbar.js
@@ -0,0 +1,32 @@
+// 底部栏组件
+
+import React from 'react';
+import {Icon as Aicon, } from 'antd';
+import {TabBar, } from 'antd-mobile';
+
+
+class Footbar extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+
+  render() {
+    return (
+      <List>
+        {this.props.menus.map((menu, index) => {
+          return (
+            <List.Item key={index}
+              thumb={menu.thumb}
+            >
+              {menu.title}
+            </List.Item>
+          );
+        })}
+      </List>
+    );
+  }
+}
+
+
+export default TabBar;
