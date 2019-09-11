7171b9331ce9f5872f3851852b5bab9152ca9027
catonlan <catonlan@163.com>
2019-09-11 16:07:45 +0800

    添加 user Reducer

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,13 +1,15 @@
 
 // 吧多个 reducer 函数合并成一个 reducer 函数
 import {combineReducers, } from 'redux';
-import {connectRouter,} from 'connected-react-router';
+import {connectRouter, } from 'connected-react-router';
 import slidebar from './reducers/slidebar';
 import footer from './reducers/footer';
+import user from './reducers/user';
 
 
 export default (history) => combineReducers({
   router: connectRouter(history),
   slidebar,
   footer,
+  user,
 });
