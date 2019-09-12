54c07800c49f65c926cee075f0d98e97c6f5328c
catonlan <catonlan@163.com>
2019-09-12 12:19:59 +0800

    BattleResult 必须有选手信息，否则跳回battle页面

--- src/pages/BattleResult.js
+++ src/pages/BattleResult.js
@@ -1,20 +1,52 @@
 import React from 'react';
 import {connect, } from 'react-redux';
 import api from '../api';
+import {Toast, } from 'antd-mobile';
+import {push, } from 'connected-react-router';
 
 
 class BattleResult extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      winner: null,
+      loser: null,
+      show: false,
+    };
+  }
+
+
   // 组件挂载后 （插入 DOM树种）立即调用
   componentDidMount() {
-    api.getUserData('kakuilan');
+    this.doCompare();
   }
-  render() {
-    return (
-      <div>
+
+  // 执行比较
+    doCompare = () => {
+      if (!this.props.battle.player_a_status) {
+        Toast.fail('选手A不存在！', 2);
+        this.props.inputCompare();
+        return;
+      } else if (!this.props.battle.player_b_status) {
+        Toast.fail('选手B不存在！', 2);
+        this.props.inputCompare();
+        return;
+      }
+
+      Toast.loading('loading...', 0);
+      const res = api.battleCompare([this.props.battle.player_a_name, this.props.battle.player_b_name, ]);
+
+      console.log('res:', res);
+      return;
+    }
+
+    render() {
+      return (
+        <div>
                 结果
-      </div>
-    );
-  }
+        </div>
+      );
+    }
 }
 
 
@@ -25,4 +57,16 @@ const mapStateToProps = (state) => {
   };
 };
 
-export default connect(mapStateToProps)(BattleResult);
+// 绑定分发器
+const mapDispatchToProps = (dispatch) => {
+  return {
+    // 跳转到输入页面
+    inputCompare: () => {
+      setTimeout(() => {
+        dispatch(push('/battle'));
+      }, 2000);
+    },
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(BattleResult);
