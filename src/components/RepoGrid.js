e1b94f34ecf4db61cc2ea5506befcf10ae10235c
catonlan <catonlan@163.com>
2019-09-13 18:09:08 +0800

    RepoGrid 使用懒加载

--- src/components/RepoGrid.js
+++ src/components/RepoGrid.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import animate from '@jam3/gsap-promise';
+// import animate from '@jam3/gsap-promise';
 
+import LazyLoad from 'react-lazy-load';
 
 class RepoGrid extends React.Component {
   constructor(props) {
@@ -14,32 +15,32 @@ class RepoGrid extends React.Component {
 
 
   componentDidMount() {
-    animate.staggerFrom(
-      '.popular-item',
-      0.5,
-      {
-        x: -1500,
-        opacty: 0.5,
-        delay: 0.2,
-      },
-      0.035
-    );
+    // animate.staggerFrom(
+    //   '.popular-item',
+    //   0.5,
+    //   {
+    //     x: -1500,
+    //     opacty: 0.5,
+    //     delay: 0.2,
+    //   },
+    //   0.035
+    // );
     // setState更新的数据必须是对象
-    // this.setState({mounted: true, });
+    this.setState({mounted: true, });
   }
 
   // 组件更新之后触发
   componentDidUpdate() {
-    animate.staggerFrom(
-      '.popular-item',
-      0.5,
-      {
-        x: -1500,
-        opacity: 0.5,
-        delay: 0.2,
-      },
-      0.035
-    );
+    // animate.staggerFrom(
+    //   '.popular-item',
+    //   0.5,
+    //   {
+    //     x: -1500,
+    //     opacity: 0.5,
+    //     delay: 0.2,
+    //   },
+    //   0.035
+    // );
   }
 
   render() {
@@ -56,10 +57,14 @@ class RepoGrid extends React.Component {
               <div className="popular-rank">#{index + 1}</div>
               <ul className="space-list-items">
                 <li>
-                  <img alt={`Avatar for ${repo.owner.login}`}
-                    className="avatar"
-                    src={repo.owner.avatar_url}
-                  />
+                  <LazyLoad height={150}
+                    offsetVertical={200}
+                  >
+                    <img alt={`Avatar for ${repo.owner.login}`}
+                      className="avatar"
+                      src={repo.owner.avatar_url}
+                    />
+                  </LazyLoad>
                 </li>
 
                 <li>
@@ -68,7 +73,7 @@ class RepoGrid extends React.Component {
                   </a>
                 </li>
                 <li>@{repo.owner.login}</li>
-                <li>@{repo.stargazers_count} stars</li>
+                <li>{repo.stargazers_count} stars</li>
               </ul>
             </li>
           );
