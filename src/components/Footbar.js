a36fe10e33de5eed61317faa28c627d0793dc7cc
catonlan <catonlan@163.com>
2019-09-13 15:46:57 +0800

    fix

--- src/components/Footbar.js
+++ src/components/Footbar.js
@@ -48,7 +48,7 @@ class Footbar extends React.Component {
       <TabBar
         barTintColor="white"
         hidden={this.props.footer.footerHidden}
-        tabBarPosition="bottom"
+        // tabBarPosition="bottom"
         tintColor="#33A3F4"
         unselectedTintColor="#949494"
       >
@@ -57,35 +57,35 @@ class Footbar extends React.Component {
           data-seed="logId"
           icon={<Aicon style={{ fontSize: '20px', }}
             type="home"
-          />}
+                />}
           key="Home"
           onPress={() => this.props.switchFooterTab('home')}
           selected={this.state.footerActive === 'home'}
           selectedIcon={<Aicon style={{ fontSize: '20px', }}
             theme="twoTone"
             type="home"
-          />}
+                        />}
           title="首页"
         >
         </TabBar.Item>
         <TabBar.Item
           icon={<Aicon style={{ fontSize: '20px', }}
             type="compass"
-                />}
+          />}
           key="Battle"
           onPress={() => this.props.switchFooterTab('battle')}
           selected={this.state.footerActive === 'battle' || this.state.footerActive === 'battle/result'}
           selectedIcon={<Aicon style={{ fontSize: '20px', }}
             theme="twoTone"
             type="compass"
-                        />}
+          />}
           title="对比"
         >
         </TabBar.Item>
         <TabBar.Item
           icon={<Aicon style={{ fontSize: '20px', }}
             type="fire"
-          />}
+                />}
           key="Popular"
           onPress={() => this.props.switchFooterTab('Popular')}
           selected={this.state.footerActive === 'Popular'}
@@ -98,14 +98,14 @@ class Footbar extends React.Component {
         <TabBar.Item
           icon={<Aicon style={{ fontSize: '20px', }}
             type="question-circle"
-          />}
+                />}
           key="Search"
           onPress={() => this.props.switchFooterTab('search')}
           selected={this.state.footerActive === 'search'}
           selectedIcon={<Aicon style={{ fontSize: '20px', }}
             theme="twoTone"
             type="question-circle"
-          />}
+                        />}
           title="搜索"
         >
         </TabBar.Item>
