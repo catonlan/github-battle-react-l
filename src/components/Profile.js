9867348001edebfe04f47d695818765b25fa0cc6
catonlan <catonlan@163.com>
2019-09-14 10:22:25 +0800

    fix

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -17,6 +17,7 @@ class Profile extends React.Component {
       <PlayerPreview avatar={info.avatar_url}
         username={info.login}
       >
+        {/** PlayerPreview的子元素 */}
         <ul className="space-list-items">
           <li><span className="itemName">Followers: </span><span className="itemVal">{info.followers}</span></li>
           <li><span className="itemName">Following: </span><span className="itemVal">{info.following}</span></li>
@@ -25,7 +26,7 @@ class Profile extends React.Component {
           {/** 以下属性,若存在,则显示 */}
           {info.name && <li><span className="itemName">User:</span><span className="itemVal">{info.name}</span></li>}
           {info.location && <li><span className="itemName">Location:</span><span className="itemVal">{info.location}</span></li>}
-          {info.company && <li><span className="itemName">Company</span><span className="itemVal">{info.company}</span></li>}
+          {info.company && <li><span className="itemName">Company:</span><span className="itemVal">{info.company}</span></li>}
           {info.blog && (
             <li>
               <span className="itemName">博客：</span><span className="itemVal">{info.blog}</span>
@@ -38,7 +39,7 @@ class Profile extends React.Component {
   }
 }
 
-Profile.PropTypes = {
+Profile.propTypes = {
   info: PropTypes.object.isRequired,
 };
 
